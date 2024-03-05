01f271d5af4c5f937e6485eab2b293f1ab84ad7c
Stefano <stefanop21@outlook.es>
2024-03-05 12:50:01 -0500

    config(apps/client): React-router, Redux Toolkit, Material UI

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import './styles.css';
+import { ExchangeApp } from './ExchangeApp.tsx';
+import { store } from './context/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <ExchangeApp />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
