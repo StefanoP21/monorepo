3af6b74a65b35789143181438b5c69ef0142e007
Stefano <stefanop21@outlook.es>
2024-03-07 22:07:19 -0500

    type(apps/client/src/types/auth/user.ts): Tipos de usuario

---
+++ apps/client/src/types/auth/user.ts
@@ -0,0 +1,14 @@
+export type RegisterUserProps = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type LoginUserProps = {
+  email: string;
+  password: string;
+};
+
+export type TokenProps = {
+  token: string;
+};
