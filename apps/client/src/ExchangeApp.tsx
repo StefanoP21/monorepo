d8b1388cbfea867215dd20f2d6db564a7d282dc8
Stefano <stefanop21@outlook.es>
2024-03-07 21:58:22 -0500

    config(apps/client/src/ExchangeApp.tsx): Tema y rutas

---
+++ apps/client/src/ExchangeApp.tsx
@@ -0,0 +1,10 @@
+import { AppRouter } from './router/AppRouter';
+import { AppTheme } from './theme/AppTheme';
+
+export const ExchangeApp = () => {
+  return (
+    <AppTheme>
+      <AppRouter />
+    </AppTheme>
+  );
+};
